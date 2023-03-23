2802f330e9a445130377717ff3c674f2475ea657
junghs1040 <junghs1040@naver.com>
2023-03-23 13:11:37 +0900

    03.23-1

---
+++ auto_kickboard_libs/src/auto_kickboard.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "ros/ros.h"
+//#include "auto_kickboard/auto_kickboard.hpp"
+
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "auto_kickboard");
+    
+    ros::NodeHandle nh("");
+    ros::NodeHandle nh_priv("~");
+    cout << "TEST" << endl;
+
+    
+    //Mintchoco1 mincho(&nh, &nh_priv);
+    ros::spin();
+    return 0;
+}
